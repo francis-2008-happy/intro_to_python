f072e6a319e36e0af4d913329569efb405a20e56
francis-2008-happy <francisbucci2@gmail.com>
2025-05-10 21:54:03 +0100

    new file added

---
+++ clever_stories.py
@@ -0,0 +1,21 @@
+print("Please enter the following: ")
+
+adjective = input("adjective: ")
+animal = input("animal: ") 
+verb = input("verb: ") 
+exclamation = input("exclamation: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+noun = input("noun: ")
+verb3 = input("verb3: ")
+noun2 = input("noun2: ")
+
+print("Your story is:  ")
+print(f"""The other day, I was really in trouble. It all started when I saw a very
+{adjective} {animal} {verb} down the hallway. "{exclamation}!" I yelled. But all
+I could think to do was to {verb1} over and over. Miraculously,
+that caused it to stop, but not before it tried to {verb2}
+right in front of my family.  2 weeks later i traveled back to {noun}, i meet some of my
+friends and they {verb3} to come give me a very big hug, we all smiled and went back to your {noun2}""")
+print()
+print("Thank you for listening to my stories")
